689a944ceb431d77fc905e6c3b6f39f3e495b56c
vineet dixit <vineetdixit.vns@gmail.com>
2024-07-06 05:04:53 -0700

    husky check

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,53 +1,54 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Drawer } from "expo-router/drawer";
-import { DrawerItem } from "@react-navigation/drawer";
-import { router } from "expo-router";
-import { useRoute } from "@react-navigation/native";
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { DrawerItem } from '@react-navigation/drawer';
+import { router } from 'expo-router';
+import { useRoute } from '@react-navigation/native';
 
 const DrawerLayout = () => {
-  const route = useRoute();
-  const { session } = route.params;
-  const sessionData = JSON.stringify(session);
-  console.log(session, "=-===");
-  const CustomDrawerComponent = () => {
-    return (
-      <>
-        <DrawerItem
-          label={"home"}
-          onPress={() => router.push("/(drawer)/(tabs)")}
-        />
-        <DrawerItem
-          label={"profile"}
-          onPress={() =>
-            router.push({
-              pathname: "/(drawer)/(tabs)/profile",
-              params: sessionData,
-            })
-          }
-        />
-        <DrawerItem
-          label={"scrren outside tabs 1"}
-          onPress={() => router.push("/outside1")}
-        />
-        <DrawerItem
-          label={"scrren outside tabs 2"}
-          onPress={() => router.push("/outside2")}
-        />
-        <DrawerItem
-          label={"reel"}
-          onPress={() => router.push("/(drawer)/(tabs)/reel")}
-        />
-      </>
-    );
-  };
+    const route = useRoute();
+    const { session } = route.params;
+    const sessionData = JSON.stringify(session);
+    console.log(session, '=-=========');
+    const CustomDrawerComponent = () => {
+        return (
+            <>
+                <DrawerItem
+                    label={'home'}
+                    onPress={() => router.push('/(drawer)/(tabs)')}
+                />
+                <DrawerItem
+                    label={'profile'}
+                    onPress={() =>
+                        router.push({
+                            pathname: '/(drawer)/(tabs)/profile',
+                            params: sessionData,
+                        })
+                    }
+                />
+                <DrawerItem
+                    label={'scrren outside tabs 1'}
+                    onPress={() => router.push('/outside1')}
+                />
+                <DrawerItem
+                    label={'scrren outside tabs 2'}
+                    onPress={() => router.push('/outside2')}
+                />
+                <DrawerItem
+                    label={'reel'}
+                    onPress={() => router.push('/(drawer)/(tabs)/reel')}
+                />
+            </>
+        );
+    };
 
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        drawerContent={(props) => <CustomDrawerComponent {...props} />}
-      ></Drawer>
-    </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                drawerContent={(props) => <CustomDrawerComponent {...props} />}
+            ></Drawer>
+        </GestureHandlerRootView>
+    );
 };
 
 export default DrawerLayout;
