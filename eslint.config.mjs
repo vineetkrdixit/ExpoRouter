f4fc68d80ced64e2aadcb868c628b11a1fc319aa
vineet dixit <vineetdixit.vns@gmail.com>
2024-07-06 02:56:17 -0700

    check husky 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
 ];
